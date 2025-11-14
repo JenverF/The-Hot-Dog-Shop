6b14ed35533e9a86ea425e0afce1b30614ef8d37
JenverF <jenver.dfernandez@gmail.com>
2025-11-13 17:18:33 -0800

    add comments for each method

--- src/main/java/com/pluralsight/Item.java
+++ src/main/java/com/pluralsight/Item.java
@@ -1,14 +1,17 @@
 package com.pluralsight;
 
+// abstract class that drink, hotdog, and side extend into because they share a name, price, and summary
 public abstract class Item {
     public String name;
     public double price;
 
+    // set the initial values
     public Item(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
+    // getters
     public String getName() {
         return name;
     }
@@ -17,5 +20,6 @@ public abstract class Item {
         return price;
     }
 
+    // abstract method for each item that overrides this to provide its own summary for the user's receipt
     public abstract String getSummary();
 }
--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -3,17 +3,20 @@ package com.pluralsight;
 import java.util.ArrayList;
 import java.util.List;
 
+// Order class that represents the customer's entire order, stores a list of item objects
 public class Order {
-    private List<Item> items;
+    private List<Item> items; // a list holding all items in the customers order
 
-    public Order() {
+    public Order() { // constructor that initializes an empty order with no items
         this.items = new ArrayList<>();
     }
 
+    // returns the list of items in the order
     public List<Item> getItems() {
         return items;
     }
 
+    // adds an item to the order as long as it is not null
     public void addItem(Item item) {
         if(item != null) {
             items.add(item);
@@ -22,6 +25,7 @@ public class Order {
         }
     }
 
+    // calculates and returns the total price of all items in the order
     public double getTotal() {
         double total = 0;
         for(Item i : items) {
@@ -30,6 +34,7 @@ public class Order {
         return total;
     }
 
+    // generates a formatted summary of all items so the user can see when they check out and saves to the receipt file
     public String displayOrder() {
         String summary = "========== ORDER SUMMARY ==========\n";
 
